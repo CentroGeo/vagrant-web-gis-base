19a5c6f0039a078ad2d2f63cd4643e90ac14d2f5
plablo <plablo09@gmail.com>
2015-03-30 19:39:20 -0600

    Lista versión inicial
    Sólo instala y configura postgres. También instala django para python3

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 PGSQL_VERSION=9.3
 
 # Need to fix locale so that Postgres creates databases in UTF-8
-cp -p /vagrant_data/etc-bash.bashrc /etc/bash.bashrc
+#cp -p /vagrant_data/etc-bash.bashrc /etc/bash.bashrc
 locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 
@@ -14,59 +14,47 @@ export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 # Install essential packages from Apt
-
-#Repositorio para PostGis, node y sus dependencias
-add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
-sudo add-apt-repository -y ppa:chris-lea/node.js
-apt-get install -y nodejs npm
-# Python dev packages
-apt-get install -y build-essential python python-dev python-setuptools python-pip
-# Dependencies for image processing with Pillow (drop-in replacement for PIL)
-# supporting: jpeg, tiff, png, freetype, littlecms
-apt-get install -y libjpeg-dev libtiff-dev zlib1g-dev libfreetype6-dev liblcms2-dev
-# Git (we'd rather avoid people keeping credentials for git commits in the repo, but sometimes we need it for pip requirements that aren't in PyPI)
-apt-get install -y git
-apt-get install -y
-
 # Postgresql
+apt-get update
 if ! command -v psql; then
     apt-get install -y postgresql-$PGSQL_VERSION libpq-dev
-    cp /vagrant_data/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
-    /etc/init.d/postgresql reload
+    cp /vagrant/install_data/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
+    cp /vagrant/install_data/postgresql.conf /etc/postgresql/$PGSQL_VERSION/main/
+    service postgresql restart
 fi
+#Password para el usuario postgres
+sudo -u postgres psql postgres -d postgres -c "ALTER USER postgres with password 'postgres';"
+#Repositorio para PostGis, node y sus dependencias
+# add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+# sudo add-apt-repository -y ppa:chris-lea/node.js
+# apt-get install -y nodejs
+# # Python dev packages
+# apt-get install -y build-essential python python-dev python-setuptools python-pip
+# # Dependencies for image processing with Pillow (drop-in replacement for PIL)
+# # supporting: jpeg, tiff, png, freetype, littlecms
+# apt-get install -y libjpeg-dev libtiff-dev zlib1g-dev libfreetype6-dev liblcms2-dev
+# # Git (we'd rather avoid people keeping credentials for git commits in the repo, but sometimes we need it for pip requirements that aren't in PyPI)
+# apt-get install -y git
+# apt-get install -y
+
+
 
 #PostGis
-apt-get install -y postgresql-server-dev-all
-apt-get install -y postgresql-$PGSQL_VERSION-postgis-2.1
-sudo -u postgres createdb postgis-template
-sudo -u postgres psql -d postgis-template -c "create extension postgis;"
-# virtualenv global setup
-if ! command -v pip; then
-    easy_install -U pip
-fi
-if [[ ! -f /usr/local/bin/virtualenv ]]; then
-    easy_install virtualenv virtualenvwrapper stevedore virtualenv-clone
-fi
+# apt-get install -y postgresql-server-dev-all
+# apt-get install -y postgresql-$PGSQL_VERSION-postgis-2.1
+# sudo -u postgres createdb postgis-template
+# sudo -u postgres psql -d postgis-template -c "create extension postgis;"
+# # virtualenv global setup
+# if ! command -v pip; then
+#     easy_install -U pip
+# fi
 
-# bash environment global setup
-cp -p /vagrant_data/bashrc /home/vagrant/.bashrc
 
-# install our common Python packages in a temporary virtual env so that they'll get cached
-if [[ ! -e /home/vagrant/.pip_download_cache ]]; then
-    su - vagrant -c "mkdir -p /home/vagrant/.pip_download_cache && \
-        virtualenv /home/vagrant/yayforcaching && \
-        PIP_DOWNLOAD_CACHE=/home/vagrant/.pip_download_cache /home/vagrant/yayforcaching/bin/pip install -r /vagrant_data/common_requirements.txt && \
-        rm -rf /home/vagrant/yayforcaching"
-fi
+# install django:
+apt-get install -y python3-pip
+pip3 install django
 
-# Node.js, CoffeeScript and LESS
 
-if ! command -v coffee; then
-    npm install -g coffee-script
-fi
-if ! command -v lessc; then
-    npm install -g less
-fi
 
 # Cleanup
 apt-get clean
